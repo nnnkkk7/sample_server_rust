b66883c11c4ba7d740750bb9b9df711fd558fe8e
nnnkkk7 <kurodanaoki0711pana@gmail.com>
2022-09-03 14:02:21 +0900

    use cargo

---
+++ projects/hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/naoki.kuroda/github/nnnkkk7/sample_server_rust/projects/hello_cargo/target/debug/hello_cargo: /Users/naoki.kuroda/github/nnnkkk7/sample_server_rust/projects/hello_cargo/src/main.rs
