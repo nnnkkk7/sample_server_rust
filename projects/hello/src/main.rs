4a3f78feeee15ba99f114d7c54ba397d60436975
nnnkkk7 <kurodanaoki0711pana@gmail.com>
2022-10-10 14:22:56 +0900

    divide into another fn

--- projects/hello/src/main.rs
+++ projects/hello/src/main.rs
@@ -8,6 +8,13 @@ fn main() {
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        println!("Connection established!");
+        handle_connection(stream);
     }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+    stream.read(&mut buffer).unwrap();
+
+    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
 }
